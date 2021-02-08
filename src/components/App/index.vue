e3690d96ed8c39377d6fa99a85d2babe1d7ac850
HitriLis <56929375+HitriLis@users.noreply.github.com>
2021-02-08 19:36:31 +0300

    Stylus instal

--- src/components/App/index.vue
+++ src/components/App/index.vue
@@ -3,3 +3,12 @@
     <router-view :key="$route.fullPath"/>
   </div>
 </template>
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './components/App'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
